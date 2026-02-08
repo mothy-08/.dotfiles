49d660695f6f38291f16202faa9a81a5e6cc2255
mothy-08 <119960367+mothy-08@users.noreply.github.com>
2026-02-08 14:21:40 +0800

    fix(oil): window splitting on modifier keys

--- config/nvim/lua/config/oil.lua
+++ config/nvim/lua/config/oil.lua
@@ -1,1 +1,10 @@
-require("oil").setup()
+require("oil").setup({
+	use_default_keymaps = false,
+	keymaps = {
+		["<CR>"] = "actions.select",
+		["-"] = { "actions.parent", mode = "n" },
+		["g."] = { "actions.toggle_hidden", mode = "n" },
+		["g?"] = { "actions.show_help", mode = "n" },
+		["gs"] = { "actions.change_sort", mode = "n" },
+	},
+})
