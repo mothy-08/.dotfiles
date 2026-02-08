fe3dc1861af7669dc0ade45d9f298fb423d32e18
mothy-08 <119960367+mothy-08@users.noreply.github.com>
2026-02-08 13:59:10 +0800

    added vue lsp

--- config/nvim/lua/config/blink.lua
+++ config/nvim/lua/config/blink.lua
@@ -1,4 +1,4 @@
 require("blink.cmp").setup({
-	fuzzy = { prebuilt_binaries = { force_version = "v1.8.0" } },
+	fuzzy = { prebuilt_binaries = { force_version = "v1.9.1" } },
 	completion = { documentation = { auto_show = true } },
 })
--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -13,7 +13,6 @@ config("lua_ls", { settings = { Lua = { workspace = { library = vim.api.nvim_get
 config("emmet_language_server", { filetypes = filetypes })
 config("tailwindcss", { filetypes = filetypes })
 config("html", { filetypes = { "html" } })
-config("vtsls", { filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" } })
 
 -- NOTE: I don't understand this for now, so I'll just comment it out
 -- config(
@@ -23,3 +22,30 @@ config("vtsls", { filetypes = { "javascript", "javascriptreact", "typescript", "
 
 -- not necessary because of mason-lspconfig
 -- vim.lsp.enable({ "lua_ls", "pyright" })
+
+-- Vue Config
+local vue_language_server_path = vim.fn.stdpath("data")
+	.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+local vue_plugin = {
+	name = "@vue/typescript-plugin",
+	location = vue_language_server_path,
+	languages = { "vue" },
+	configNamespace = "typescript",
+}
+local vtsls_config = {
+	settings = {
+		vtsls = {
+			tsserver = {
+				globalPlugins = {
+					vue_plugin,
+				},
+			},
+		},
+	},
+	filetypes = tsserver_filetypes,
+}
+
+local vue_ls_config = {}
+vim.lsp.config("vtsls", vtsls_config)
+vim.lsp.config("vue_ls", vue_ls_config)
